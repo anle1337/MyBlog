9b5d48d8110c929aefe97d6efcfdf2b0482b07a9
An Le <asble@ucdavis.edu>
2015-10-22 16:50:12 -0700

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
+	def create
+		@article = Article.new(article_param)
+
+		if @article.save
+			redirect_to @article
+			flash[:success] = "Article successfully saved!"
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit 
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_param)
+			redirect_to @article
+			flash[:success] = "Article successfully saved!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to root_path
+	end
+
+private
+
+	def article_param
+		params.require(:article).permit(:title, :body)
+	end
+end
---
+++ test/controllers/articles_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ArticlesControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
