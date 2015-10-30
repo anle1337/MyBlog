b899a91d77168c48deda6b94acc33d0905bbf199
An Le <asble@ucdavis.edu>
2015-10-29 20:22:02 -0700

    Done with contact page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,20 +6,20 @@ class MessagesController < ApplicationController
   def create
   	@message = Message.new(message_param)
 
-	if @message.valid?
-		ContactForm.new_message(@message).deliver_now
-		flash[:notice] = "Thank you for contacting us! We will get back to you within 24-48 hours."
-		redirect_to contact_url 
-	else
-		render 'new'
+		if @message.valid?
+			ContactForm.new_message(@message).deliver_now
+			flash[:success] = "Thank you for contacting me! I'll get back to you within 24-48 hours."
+			redirect_to contact_url 
+		else
+			render 'new'
+
+		end
 	end
-end
-
-  end
 
 
-private
-def message_param
-	params.require(:message).permit(:name, :email, :body, :subject)
+	private
+		def message_param
+			params.require(:message).permit(:name, :email, :body, :subject)
+		end
+	end
 
-end
